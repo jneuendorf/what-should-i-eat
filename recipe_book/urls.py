e9e21ca250606e80d9f1e6774aa82ac3ddcdec6b
Jim Neuendorf <jimneuendorf@gmx.de>
2016-10-13 01:25:34 +0200

    added urlconf namespace

--- recipe_book/urls.py
+++ recipe_book/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-
+app_name = 'recipe_book'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<recipe_id>[0-9]+)/$', views.detail, name='detail'),
