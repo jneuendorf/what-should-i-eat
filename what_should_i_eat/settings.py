5428dbbad1c0998d68b404bef16eee2030fdb5dd
Jim Neuendorf <jimneuendorf@gmx.de>
2016-10-17 00:34:27 +0200

    added sass preprocessor and test

--- what_should_i_eat/settings.py
+++ what_should_i_eat/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'colorfield',
     'jquery',
+    'sass_processor',
 
     'recipe_book',
 ]
@@ -123,3 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/Users/jimneuendorf/Developer/django/what-should-i-eat/'
+SASS_PROCESSOR_ROOT = STATIC_ROOT + 'recipe_book/static/'
+
+STATICFILES_DIRS = [
+    # shared assets among all apps
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+)
